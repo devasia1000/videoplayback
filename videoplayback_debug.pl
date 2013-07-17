f519045f8d161b4b67c17e559e024d787c5d223b
Devasia Manuel <devasia1000@gmail.com>
2013-07-17 12:53:55 -0400

    Testing with HTTPResponse object

--- videoplayback_debug.pl
+++ videoplayback_debug.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+use HTTP::Response;
+
 $params= $ENV{'QUERY_STRING'};
 @tokens=split('&', $params);
 
@@ -17,18 +19,16 @@ foreach $string (@tokens){
    @length=split("-", $range);
    $content_length=$length[1]-$length[0]+1; 
 
-   $command="tail -c +${length[0]} audio.mp4 | head -c ${content_length}";
+   $command=`tail -c +${length[0]} audio.mp4 | head -c ${content_length}`;
    
    # we don't need to print out the status, the browser handles it
    #print "HTTP/1.1 200 OK\r\n";
    
-   print "Content-Length: ", $content_length, "\r\n";
-   print "Last-Modified: Fri, 28 Jun 2013 14:34:55 GMT\r\n";
-   print "Content-Type: audio/mp4\r\n";
-   print "Date: Wed, 17 Jul 2013 14:28:52 GMT\r\n";
-   print "Expires: Wed, 17 Jul 2013 14:28:52 GMT\r\nCache-Control: private, max-age=22303\r\nAccept-Ranges: bytes\r\nAccess-Control-Allow-Origin: http://www.youtube.com\r\nAccess-Control-Allow-Credentials: false\r\nTiming-Allow-Origin: http://www.youtube.com\r\nConnection: keep-alive\r\nX-Content-Type-Options: nosniff\r\nServer: gvs 1.0\n\r\n";
+   $headers={"Content-Length: ": $content_length, "Content-Type: ", "audio/mp4", "Accept-Ranges: " : "bytes", "Connection: " : "keep-alive", "Connection: " : "keep-alive", "Server:" : "gvs 1.0", "Cache-Control: " : "private, max-age=22303", "Access-Control-Allow-Origin: " : "http://www.youtube.com", "Access-Control-Allow-Credentials: " : "false", "Timing-Allow-Origin: " : "http://www.youtube.com", "Last-Modified: " : "Fri, 28 Jun 2013 14:34:55 GMT", "Date: " : "Wed, 17 Jul 2013 14:28:52 GMT", "Expires: " : "Wed, 17 Jul 2013 14:28:52 GMT"}
 
-   print `$command`, "\r\n";
+   $resp=HTTP::Response->new("200", "", $headers, $command);
+
+   print $resp->as_string;
   }
  }
 
